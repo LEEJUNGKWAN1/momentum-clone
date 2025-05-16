aea3e36c8f6723764e2e728098c6b566c57e8121
LEEJUNGKWAN1 <wjdrhks1996@naver.com>
2025-05-17 00:15:13 +0900

    background(fullscreen)
    background(fullscreen)

--- JS/background.js
+++ JS/background.js
@@ -4,6 +4,8 @@ const chosenImage = images[Math.floor(Math.random() * images.length)];
 
 const bgImage = document.createElement("img");
 
+bgImage.classList.add("fullscreen-image"); // 클래스 지정
+
 bgImage.src = `img/${chosenImage}`;
 
 document.body.appendChild(bgImage);
